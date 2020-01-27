444685f0212ee17f6e3be11502adfacc7abf1b38
JaneAndonovski <jane.andonovski@yahoo.com>
2020-01-27 19:40:32 +0100

    initial commit

---
+++ advance.js
@@ -0,0 +1,21 @@
+//se sobirat baranjata, e pravi dizajn na resenie, pa se pisuva kod, pa testiranje dali ona sto sme go napravile gi zadovoluva startnite baranja
+//aplikacija samo na desktop da se prikazuva da ne bi responsive ; single paige aplikacija
+//web aplikacija za vreme, na pomali delovi i edno po edno da gi pravime
+//OpenWeather :JaneAndonovski Janesedc1994
+
+ // Plan za rabota 
+// Tasks for WeatherAlert Application
+// 1.Create html page (index.html), js file(app.js), style.css and link them into html page
+// 2.Add bootstrap to index.html
+// 3.Create navbar with links to 3 od the needed pages (Home - statistics, hourly-weather, about)
+// 4.Add logo and aplication name into navbar
+// 5.Add 3 pages (as divs to the html)
+// 6. Add js logic for navbar links
+// 7. Create first page details to html (Welcome  message and text)
+// 7.1 Make ajax call to (https://openweathermap.org/) (find the appropriate url), get the needed data, aggregateit and display it into html
+// 8.Create second page details(hourly data)(tabela so podatoci tie podatoci ke gi zememe so ajax povik do api(https://openweathermap.org/)) and display into html table
+// 9. Create about page details(only html info about us)
+
+
+
+
